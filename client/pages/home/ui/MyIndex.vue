43643700b46b13b3fa88b991106e1ece3234f3a4
awesomex <bestawes0mex@gmail.com>
2024-08-18 21:25:48 +0500

    nuxt template

---
+++ client/pages/home/ui/MyIndex.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+
+// example composables
+const { sayExample } = useExample();
+sayExample();
+
+onMounted(() => {
+  // example plugins
+  nuxtApp.$sayHello();
+});
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style scoped></style>
---
+++ client/shared/ui/layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/">Contacts</NuxtLink>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+</template>
+
+<style scoped></style>
